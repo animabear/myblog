4f70845c4e7b3a8be03887d88122d3951e90eae4
animabear <whuxjj92@gmail.com>
2013-11-06 21:04:53 +0800

    add single,add main loop,add post_views

--- functions.php
+++ functions.php
@@ -9,4 +9,31 @@
 
 	/* 开启菜单功能 - dashboard 可见 */
 	register_nav_menus(array('primary' => '一级导航'));
+
+	/* 浏览次数统计 */
+	/* 加到single.php的主循环中 */
+	function setPostViews ($postID) {
+		$count_key = 'post_views_count';
+		$count = get_post_meta($postID, $count_key, true);
+		if($count == ''){ //没有内容返回
+			$count = 0;
+			delete_post_meta($postID, $count_key);
+			add_post_meta($postID, $count_key, '0');
+		}else{
+			$count++;
+			update_post_meta($postID, $count_key, $count);
+		}
+	}
+	/* 加到需要获取浏览数的地方 */
+	function getPostViews ($postID) {
+		$count_key = 'post_views_count';
+		$count = get_post_meta($postID, $count_key, true);
+		if($count == ''){ //没有内容返回
+			delete_post_meta($postID, $count_key);
+			add_post_meta($postID, $count_key, 0);
+			return 0;
+		}
+		return $count;
+	}
+
 ?>
--- single.php
+++ single.php
@@ -3,16 +3,10 @@
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 		<?php if (have_posts()): ?>
-			<?php while (have_posts()): the_post(); ?>
+			<?php while (have_posts()): the_post(); setPostViews(get_the_ID()); ?>
 			<article class="post" id="post-<?php the_ID(); ?>">
 				<div class="post-header">
 					<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-					<?php if (getPostViews(get_the_ID()) > 0): ?>
-					<div class="post-viewed" title="被围观的次数">
-						<!-- 输出文章的浏览次数 -->
-						<?php echo getPostViews(get_the_ID()); ?>
-					</div>
-					<?php endif; ?>
 				</div>
 				<div class="post-meta">
 					<?php the_category(' | ') ?>
