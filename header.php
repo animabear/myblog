4a108bb2448e5d2bf9a5b8ce8681e7994af02d80
animabear <whuxjj92@gmail.com>
2013-11-09 21:28:56 +0800

    add links and weibo show

--- header.php
+++ header.php
@@ -3,15 +3,16 @@
 <head>
 <title>
 <?php
-if (is_home()):
-bloginfo('name'); echo " | "; bloginfo('description');
-else: 
-wp_title('|', true, right); bloginfo('name');
-endif;
+	if (is_home()):
+		bloginfo('name'); echo " | "; bloginfo('description');
+	else: 
+		wp_title('|', true, right); bloginfo('name');
+	endif;
 ?>
 </title>
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/normalize.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+
 <?php wp_head(); ?> <!-- 输出插件依赖的头信息 -->
 </head>
 
--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,9 @@
 <div id="sidebar">
 	<!-- 新浪微博 -->
-	
+	<aside class="widget">
+		<h3>新浪微博</h3>
+		<iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=2&ptype=0&speed=0&skin=1&isTitle=0&noborder=0&isWeibo=1&isFans=0&uid=1801486485&verifier=985d08b5&dpc=1"></iframe>
+	</aside>
 	<?php 
 		if (function_exists('dynamic_sidebar') && is_active_sidebar('right-sidebar')):
 			dynamic_sidebar('right-sidebar');
